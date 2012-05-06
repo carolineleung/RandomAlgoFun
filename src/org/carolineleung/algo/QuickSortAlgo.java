9b7cea6589f24d69fb2d878c8f8139adee3102a1
Caroline Leung <calkwl@hotmail.com>
2012-05-06 14:58:54 -0700

    CarolL: Added Kth largest algo and tests

--- src/org/carolineleung/algo/QuickSortAlgo.java
+++ src/org/carolineleung/algo/QuickSortAlgo.java
@@ -38,4 +38,40 @@ public class QuickSortAlgo {
 		return left;
 	}
 
+	public List<Integer> findKLargest(List<Integer> input, int k) {
+		if (input.size() <= k) {
+			return input;
+		}
+		int midIndex = input.size() / 2;
+		int midValue = input.get(midIndex);
+		int midValueCount = 0;
+		List<Integer> left = new ArrayList<Integer>();
+		List<Integer> right = new ArrayList<Integer>();
+
+		for (int curr : input) {
+			if (curr < midValue) {
+				left.add(curr);
+			} else if (curr > midValue) {
+				right.add(curr);
+			} else {
+				midValueCount++;
+			}
+		}
+
+		int i = 0;
+		while (right.size() < k && i < midValueCount) {
+			right.add(midValue);
+			i++;
+		}
+		if (right.size() < k) {
+			right.addAll(findKLargest(left, k - right.size()));
+			return right;
+
+		} else if (right.size() > k) {
+			return findKLargest(right, k);
+		} else {
+			return right;
+		}
+	}
+
 }
--- test/org/carolineleung/algo/QuickSortAlgoTest.java
+++ test/org/carolineleung/algo/QuickSortAlgoTest.java
@@ -11,7 +11,7 @@ public class QuickSortAlgoTest {
 
 	@Test
 	public void testQuickSort() {
-		List<Integer> intList = InputFactory.createIntegerList(new int[] { 3, 5, 7, 1, -1, 2, 1, 5, 6 });
+		List<Integer> intList = InputFactory.createIntegerList(new int[] { 3, 5, 7, 1, 4, -1, 2, 1, 5, 6 });
 		QuickSortAlgo algo = new QuickSortAlgo();
 		List<Integer> sortedList = algo.quickSort(intList);
 
@@ -20,5 +20,17 @@ public class QuickSortAlgoTest {
 
 		Assert.assertTrue(intList.containsAll(sortedList));
 		Assert.assertTrue(sortedList.containsAll(intList));
+	}
+
+	@Test
+	public void testFindKLargest() {
+		List<Integer> intList = InputFactory.createIntegerList(new int[] { 3, 5, 7, 1, 4, -1, 2, 1, 5, 6 });
+		QuickSortAlgo algo = new QuickSortAlgo();
+
+		for (int i = 1; i <= intList.size(); i++) {
+			List<Integer> result = algo.findKLargest(intList, i);
+			System.out.println("Largest " + i + " is: " + result);
+		}
+
 	}
 }
